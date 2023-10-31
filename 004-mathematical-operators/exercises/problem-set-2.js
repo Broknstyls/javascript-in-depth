ea5a933c287a20f43818a9c94062010c3b3b69f7
Broknstyls <muban90@gmail.com>
2023-10-31 22:05:39 +0200

    solved 04 01&&02

--- 004-mathematical-operators/exercises/problem-set-2.js
+++ 004-mathematical-operators/exercises/problem-set-2.js
@@ -6,3 +6,7 @@
     Question1: What are all the ways you can achieve step 3 above?
     Question2: What would happen if you tried to do this with a const variable?
 */
+let points;
+console.log(points);
+points =15;
+console.log(points);
